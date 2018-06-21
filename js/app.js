509cae6658d92dc813a33f51d765b17aa2304c21
Alexandros Pekos <apekos7@gmail.com>
2018-06-21 04:54:38 +0300

    Created Player class, filled Enemy constractor function + update method and instantiated my objects.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    // Enemies cross the border
+    if (this.x > 505) {
+        this.x = -100;
+        //Randomizing the speed after crossing the border
+        this.speed = 83 + Math.floor(Math.random() * 200);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +34,58 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-boy.png';
+    }
 
+    update(dt) {
+
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Checks which key was pressed and moves the player 
+    // without crossing the borders.
+    handleInput(key) {
+        switch (key) {
+            case 'left': if (this.x > 0) {
+                this.x -= 101;
+            }
+            break; 
+
+            case 'up': if (this.y > 0) {
+                this.y -= 83;
+            } else {
+                this.y = 404;
+            }
+            break;
+
+            case 'right': if (this.x < 400) {
+                this.x += 101;
+            }
+            break;
+
+            case 'down': if (this.y < 400) {
+                this.y += 83;
+            }
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemy1 = new Enemy(-100, 230, 100);
+let enemy2 = new Enemy(-100, 145, 150);
+let enemy3 = new Enemy(-100, 65, 200);
+let player = new Player(202, 404);
 
-
+allEnemies = [enemy1, enemy2, enemy3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
